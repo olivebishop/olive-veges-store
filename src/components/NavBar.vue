67238305337585e8da28c6df18cfe095c3085472
luqmanshaban <luqmanforeal@gmail.com>
2023-07-19 22:36:04 +0300

    first commit -creating the navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav>
+      <RouterLink to="/">Logo</RouterLink>
+      <ul :class="{ active: active, navMenu: true }">
+        <li @click="unToggle">
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li @click="unToggle">
+          <RouterLink to="/product">Product</RouterLink>
+        </li>
+        <li @click="unToggle">
+          <a id="contact">Contact</a>
+        </li>
+      </ul>
+      <ul :class="{active: active, buttons: true}">
+        <li @click="unToggle" class="btn1">
+          <button class="signup">Sign Up</button>
+        </li>
+        <li @click="unToggle" class="btn2">
+          <button class="login">Login</button>
+        </li>
+      </ul>
+      <div :class="{ hamburger: true, active: active }" @click="toggle">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  import { RouterLink } from 'vue-router';
+  import '../assets/styles/Navbar.scss'
+  
+  export default {
+    name: 'NavBar',
+    components: {
+      RouterLink,
+    },
+  
+    data() {
+      return {
+        active: false,
+      };
+    },
+  
+    methods: {
+      toggle() {
+        this.active = !this.active; //
+      },
+      unToggle() {
+        this.active = false;
+      }
+    },
+  };
+  </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import '../assets/styles/Home.scss'
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+  </main>
+</template>
