b7d14664ce9d4b737fc55a4b42aee6ff33ee7325
luqmanshaban <luqmanforeal@gmail.com>
2023-07-24 09:28:40 +0300

    adding the login/signup form

---
+++ src/views/SignupView.vue
@@ -0,0 +1,44 @@
+<script setup>
+import '@/assets/styles/Signup.scss'
+import { ref } from 'vue';
+import axios from 'axios';
+
+const user = ref({
+  username: '',
+  email: '',
+  password: ''
+})
+
+const submitForm = async () => {
+  try {
+    const response = await axios.post('http://localhost:8000/api/signup', user.value);
+    console.log(response);
+   
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+
+<template>
+  <div class="signup-form">
+    <form @submit.prevent="submitForm">
+      <fieldset>
+        <legend>Sign Up</legend>
+        <div>
+          <label for="username">Username:</label><br>
+          <input type="text" id="username" v-model="user.username">
+        </div>
+        <div>
+          <label for="email">Email:</label><br>
+          <input type="email"  id="email" v-model="user.email">
+        </div>
+        <div>
+          <label for="password">Password:</label><br>
+          <input type="password"  id="password" v-model="user.password">
+        </div>
+        <button type="submit">Sign Up</button>
+      </fieldset>
+    </form>
+  </div>
+</template>
